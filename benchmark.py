bceb10d913c3e180a497a115fcf01f39717d1df8
bawaji94 <bawaji94@gmail.com>
2020-12-18 15:08:35 +0530

    remove files after each run

--- benchmark.py
+++ benchmark.py
@@ -35,6 +35,7 @@ def benchmark_data(file_format, write_file, read_file, library, engine):
         read_file()
         read_time_end = time.time()
         read_times.append(read_time_end - read_time_start)
+        os.remove(file_name(file_format))
 
     file_size_in_bytes = os.path.getsize(file_name(file_format))
     file_size = convert_to_MB(file_size_in_bytes)
