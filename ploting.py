b5f92d12f2eb221aa9e43f19c627d8da4149f5a6
bawaji94 <bawaji94@gmail.com>
2020-12-19 11:02:38 +0530

    add bar plot for the time and size

---
+++ ploting.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def anotate_value(ax, postfix=''):
+    for p in ax.patches:
+        ax.annotate(str(round(p.get_height(), 2))+postfix, 
+                    (p.get_x() + p.get_width() / 2., p.get_height()), 
+                    ha = 'center', va = 'center', fontsize=8,
+                    xytext = (0, 9), 
+                    textcoords = 'offset points')
+
+df = pd.read_csv('data_file_format_benchmark.csv')
+df = df[(df.fileFormat!='csv') | (df.library != 'pandas')]
+
+ax_read_write = df.plot.bar(x='fileFormat', y=['write_time', 'read_time'], rot=30, xlabel='')
+anotate_value(ax_read_write, postfix='s')
+
+ax_storage_size = df.plot.bar(x='fileFormat', y='sizeOnDisk', color='#ff7f0e', rot=30, xlabel='', legend=False)
+anotate_value(ax_storage_size, postfix='MB')
+ax_storage_size.set_ylabel('Size of file on disk in MB', fontsize=12)
+plt.show()
